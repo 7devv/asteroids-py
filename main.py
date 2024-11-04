fa13bb5ba0cc3fe9c854af24ed273f16599b9092
7devv <7devpotter@gmail.com>
2024-11-03 22:43:40 -0800

    Add player triangle render

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     game_clock = pygame.time.Clock()
-    
     dt = 0
     
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     
     while True:
         for event in pygame.event.get():
@@ -16,13 +17,18 @@ def main():
                 return    
     
         pygame.Surface.fill(screen, (0, 0, 0))
-        pygame.display.flip()
-        dt = game_clock.tick(60) / 1000
         
+        player.draw(screen)
+        
+        pygame.display.flip()
+        
+        dt = game_clock.tick(60) / 1000
+
+ 
     
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")    
-    print(f"Screen height: {SCREEN_HEIGHT}")
+print("Starting asteroids!")
+print(f"Screen width: {SCREEN_WIDTH}")    
+print(f"Screen height: {SCREEN_HEIGHT}")
 
     
 if __name__ == "__main__":
